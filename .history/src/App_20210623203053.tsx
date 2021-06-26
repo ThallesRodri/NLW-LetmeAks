10499ac314b294a172661866963ab1db10cfe5b2
ThallesRodri <thalles07rodri@gmail.com>
2021-06-26 14:11:53 -0300

    Commit inicial

---
+++ .history/src/App_20210623203053.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+import { AuthContextProvider } from './context/AuthContext';
+import { Room } from "./pages/Room";
+
+function App() {
+
+  return (
+    <BrowserRouter> 
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/news" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
+      </AuthContextProvider>       
+    </BrowserRouter>
+  );
+}
+
+export default App;
